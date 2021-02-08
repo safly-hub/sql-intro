44eb7f87a4db629b46a979ff1f0f882d8f690f05
LUCAS SAFLY <saflycorp@gmail.com>
2021-02-08 11:04:36 -0500

    queries.sql content

---
+++ queries.sql
@@ -0,0 +1,19 @@
+Create a database: CompanyDatabase.
+CREATETABLE "Employees" ("Full Name"TEXTNOTNULL,"Salary"INT,"Job Position"TEXT,"Phone Extension"INT,"Is Part Time");
+
+ Create a table: Employees with the following columns. Use your best judgment for data types. Copy the query you used to create this table and paste it into queries.sql.
+//FullName
+Salary
+JobPosition
+PhoneExtension
+IsPartTime
+ Create a few employees. Review the requirements below to see some examples of the values you may need. For instance, you may need employees of certain job positions, employees with certain names, etc. Add enough data (maybe 10 rows) such that you can validate your queries. Copy the queries you used to create your data into queries.sql
+
+ Create queries that can do the following. Each query should be attempted in your database. When it works, copy the query to queries.sql. You may optionally copy and paste the query output to your queries.sql if you wish.
+
+ Select all columns for all employees.
+ Select only the full names and phone extensions for only full-time employees.
+ Insert a new part-time employee, as a software developer, with a salary of 450. Make up values for the other columns.
+ Update all employees that are cooks to have a salary of 500.
+ Delete all employees that have the full name of "Lazy Larry".
+ Add a column to the table: ParkingSpot as textual information that can store up to 10 characters.
