1b0aee74e9a71da77fd7a05e2e837d4aa9974d9a
LUCAS SAFLY <saflycorp@gmail.com>
2021-02-09 11:45:56 -0500

    initial commit

--- queries.sql
+++ queries.sql
@@ -1,19 +1,61 @@
-Create a database: CompanyDatabase.
-CREATETABLE "Employees" ("Full Name"TEXTNOTNULL,"Salary"INT,"Job Position"TEXT,"Phone Extension"INT,"Is Part Time");
+-- Create a Table
+CREATE TABLE "Employees"
+(
+    "Full Name" TEXT,
+    "Salary" INT,
+    "Job Position" TEXT,
+    "Phone Extension" INT,
+    "Is Part Time" BOOL
+);
+-- Create Employees
+INSERT INTO "Employees"
+    ("Full Name","Salary","Job Position","Phone Extension","Is Part Time")
+VALUES
+    ('John Jackson', '200000', 'Coffee Courier', '000', 'true');
+INSERT INTO "Employees"
+    ("Full Name","Salary","Job Position","Phone Extension","Is Part Time")
+VALUES
+    ('Todd Ault', '1000000', 'CEO', '001', 'false');
+INSERT INTO "Employees"
+    ("Full Name","Salary","JobPosition","Phone Extension","Is Part Time")
+VALUES
+    ('Perry Williams’,'150000',‘CTO’,‘333’,‘false’);
+INSERT INTO "Employees" ("Full Name","Salary","Job Position","Phone Extension","Is Part Time")
+VALUES ('Lazy Larry','25000','Son of the boss','444','true');
+INSERT INTO "Employees" ("Full Name","Salary","Job Position","Phone Extension","Is Part Time")
+VALUES ('Alex Jones’, ‘70000’, ‘PR Manager’, ‘555’, ‘false’);
+INSERT INTO "Employees"
+    ("Full Name","Salary","Job Position","Phone Extension","Is Part Time")
+VALUES
+    ('Anthony Pettis', '70000', 'Cook', '666', 'false');
 
- Create a table: Employees with the following columns. Use your best judgment for data types. Copy the query you used to create this table and paste it into queries.sql.
-//FullName
-Salary
-JobPosition
-PhoneExtension
-IsPartTime
- Create a few employees. Review the requirements below to see some examples of the values you may need. For instance, you may need employees of certain job positions, employees with certain names, etc. Add enough data (maybe 10 rows) such that you can validate your queries. Copy the queries you used to create your data into queries.sql
+-- Select all columns for all employees.
+CompanyDatabase>
+SELECT *
+FROM "Employees";
 
- Create queries that can do the following. Each query should be attempted in your database. When it works, copy the query to queries.sql. You may optionally copy and paste the query output to your queries.sql if you wish.
 
- Select all columns for all employees.
- Select only the full names and phone extensions for only full-time employees.
- Insert a new part-time employee, as a software developer, with a salary of 450. Make up values for the other columns.
- Update all employees that are cooks to have a salary of 500.
- Delete all employees that have the full name of "Lazy Larry".
- Add a column to the table: ParkingSpot as textual information that can store up to 10 characters.
+
+-- Select only the full names and phone extensions for only full-time employees.
+SELECT "Full Name", "Phone Extension"
+FROM "Employees"
+WHERE "Is Part Time" = 'false';
+-- Insert a new part-time employee, as a software developer, with a salary of 450. Make up values for the other columns.
+INSERT INTO "Employees"
+    ("Full Name","Salary","Job Position","Phone Extension","Is Part Time")
+VALUES
+    ('Tristan DeAugustine', '450000', 'Senior Developer', '777', 'true');
+-- Update all employees that are cooks to have a salary of 500.
+UPDATE "Employees" SET "Salary" = '500000' WHERE "Job Position" = 'Cook';
+-- Delete all employees that have the full name of “Lazy Larry”.
+DELETE FROM "Employees" WHERE "Full Name" = 'Lazy Larry';
+--Add a column to the table: ParkingSpot as textual information that can store up to 10 characters.
+ALTER TABLE "Employees" ADD COLUMN “ParkingSpot” VARCHAR
+(10);
+-- In your CompanyDatabase, add a table named Departments with the following columns:
+CREATE TABLE “Departments”
+(
+ "Id" SERIAL PRIMARY KEY,
+ "DepartmentName" INT,
+ "Building" TEXT
+ );
